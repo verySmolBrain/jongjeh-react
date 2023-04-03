426213e738a590522979b1f6a27cb73d98fe36a8
smolbrain <randomlyrandamu@gmail.com>
2023-04-03 21:46:31 +1000

    A

--- src/components/DraggableImage.tsx
+++ src/components/DraggableImage.tsx
@@ -1,25 +1,30 @@
 import PropTypes from 'prop-types';
 import { Resizable } from 're-resizable'
 import Draggable from 'react-draggable'
+import { LinkBox, LinkOverlay, Image } from '@chakra-ui/react'
 
 function DraggableImage({ imageUrl, x, y } : { imageUrl: string, x: number, y: number }) {
 
     return (
-        <Draggable defaultPosition = {{ x: x, y: y }}>
-            <Resizable 
-                defaultSize={{ width: 500, height: 500 }}
-                style = {{
-                    background: `url(${imageUrl})`,
-                    backgroundSize: 'contain',
-                    backgroundRepeat: 'no-repeat',
-                    position: 'static',
-                    border: '1px solid #000',
-                    backgroundColor: '#fff',
-                }}
-                lockAspectRatio = { true }
-            >
-            </Resizable>
-        </Draggable>
+        <LinkBox>
+            <LinkOverlay>
+                <Draggable defaultPosition = {{ x: x, y: y }}>
+                    <Resizable 
+                        defaultSize={{ width: 500, height: 500 }}
+                        style = {{
+                            backgroundSize: 'contain',
+                            backgroundRepeat: 'no-repeat',
+                            position: 'static',
+                            border: '1px solid #000',
+                            backgroundColor: '#fff',
+                        }}
+                        lockAspectRatio = { true }
+                    >
+                        <Image src = { imageUrl } style = {{ border: '1px solid #000' }} />
+                    </Resizable>
+                </Draggable>
+            </LinkOverlay>
+        </LinkBox>
     )
 }
 
